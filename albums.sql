538094c77a7cd9db73152e0b7e0746aea5edc14e
Rachel Ralston <rachelralston@gmail.com>
2017-06-26 12:11:20 -0700

    half way through creating basic views. Completed schema and additional seed data :

--- albums.sql
+++ albums.sql
@@ -6,3 +6,17 @@ VALUES
   ('Melodrama', 'Lorde'),
   ('In Rainbows', 'Radiohead')
 ;
+
+INSERT INTO
+  users (name, email, password, profile_pic)
+VALUES
+  ('Adventure Cat', 'cat@cat.cat', 'meow', 'https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif'),
+  ('A Person', 'a@a.a', 'a', 'https://m.popkey.co/b889d7/OwGXE.gif')
+;
+
+INSERT INTO
+  reviews (album_id, user_id, review)
+VALUES
+  (1,2,'Album: Malibu. Reviewer: A. Person. I love this album so much! Its one of my all time favorites!'),
+  (2,1,'Album: A Seat at the Table. Reviewer: Miss Adventure Cat. Never again will I trust a human being.')
+;
--- schema.sql
+++ schema.sql
@@ -1,5 +1,25 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS reviews;
+
 CREATE TABLE albums (
   id SERIAL,
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE users (
+  id SERIAL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  date_joined timestamp default current_timestamp,
+  profile_pic VARCHAR(255)
+);
+
+CREATE TABLE reviews (
+  id SERIAL,
+  album_id INTEGER,
+  user_id INTEGER,
+  review TEXT
+)
